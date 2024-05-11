710d6f2ddf4c011d0cb9c8588ed59bc54fbb0bf3
gavhart <har19076@byui.edu>
2024-05-11 12:48:47 -0600

    fixed

--- server.js
+++ server.js
@@ -12,28 +12,32 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
-const utilities = require("./utilities")
+const utilities = require("./utilities/")
 
-/* ***********************
- * View Engine and Templates
- *************************/
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
+
+
+
+
 
 /* ***********************
  * Routes
  *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
-// Index route
-app.get("/", utilities.handleErrors(baseController.buildHome))
-// Inventory routes
-app.use("/inv", utilities.handleErrors(inventoryRoute))
-// File Not Found Route - must be last route in list
-app.get("/error", baseController.errorHandler);
-app.use(async (req, res, next) => {
-  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+
+
+//index route
+app.get("/", function(req, res) {
+  res.render("index", { title: "Home" })
 })
+
+app.use(static)
+//Index route
+app.get("/",utilities.handleErrors(baseController.buildHome))
+//Inventory routes
+app.use("/inv", inventoryRoute)
 
 /* ***********************
 * Express Error Handler
@@ -41,14 +45,12 @@ app.use(async (req, res, next) => {
 *************************/
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
-  // let errImg = '/images/site/wait-minute.gif'
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
-  if(err.status == 404){ message = err.message, errImg='/images/site/wait-minute.gif' } else {message = 'Oh no! There was a crash. Maybe try a different route?', errImg='/images/site/rick-roll.gif'}
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
   res.render("errors/error", {
     title: err.status || 'Server Error',
     message,
-    nav,
-    errImg
+    nav
   })
 })
 
