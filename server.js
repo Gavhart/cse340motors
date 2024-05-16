7704758063f225e3630aa389c503c9a4444d3760
gavhart <har19076@byui.edu>
2024-05-15 21:14:34 -0600

    still going

--- server.js
+++ server.js
@@ -19,28 +19,13 @@ const pool = require("./database")
 const bodyParser = require("body-parser")
 
 
+
+
 /* ***********************
  * Middleware
  * ************************/
-// app.use(
-//   session({
-//     store: new (require("connect-pg-simple")(session))({
-//       createTableIfMissing: true,
-//       pool,
-//     }),
-//     secret: process.env.SESSION_SECRET,
-//     resave: true,
-//     saveUninitialized: true,
-//     name: "sessionId",
-//   })
-// )
 
-// Express Messages Middleware
-// app.use(require("connect-flash")())
-// app.use(function (req, res, next) {
-//   res.locals.messages = require("express-messages")(req, res)
-//   next()
-// })
+
 
 // Body Parser Middleware
 app.use(bodyParser.json())
@@ -56,22 +41,23 @@ app.set("layout", "./layouts/layout") // not at views root
 /* ***********************
  * Routes
  *************************/
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
-app.use(static)
+// app.use(static)
+app.use(utilities.handleErrors(static))
 
+// Index (home page) route
+app.get("/", utilities.handleErrors(baseController.buildHome))
 
-//Index route
-app.get("/",utilities.handleErrors(baseController.buildHome))
-//Inventory routes
-app.use("/inv", inventoryRoute)
-// Account Route
-// app.use("/account", accountRoute)
+// Account login
+// app.use("/account/", utilities.handleErrors(accountRoute))
+
+// Inventory route
+app.use("/inv/", utilities.handleErrors(inventoryRoute))
+
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
-  next({ status: 404, message: "Sorry, we appear to have lost that page." })
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
 })
+
 
 /* ***********************
 * Express Error Handler
@@ -80,9 +66,14 @@ app.use(async (req, res, next) => {
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
-  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  if(err.status == 404) {
+    message = err.message
+  } else {
+    message = 'Oh no! There was a crash. Maybe try a different route?'
+  }
   res.render("errors/error", {
     title: err.status || 'Server Error',
+    // message: err.message,
     message,
     nav
   })
