6b734c6f30e554b10e2a2707588ce42bb8794a23
gavhart <har19076@byui.edu>
2024-05-15 21:53:50 -0600

    hmmm

--- server.js
+++ server.js
@@ -26,23 +26,23 @@ const session = require("express-session")
 /* ***********************
  * Middleware
  * ************************/
-app.use(session({
-  store: new (require('connect-pg-simple')(session))({
-    createTableIfMissing: true,
-    pool,
-  }),
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true,
-  name: 'sessionId',
-}))
+// app.use(session({
+//   store: new (require('connect-pg-simple')(session))({
+//     createTableIfMissing: true,
+//     pool,
+//   }),
+//   secret: process.env.SESSION_SECRET,
+//   resave: true,
+//   saveUninitialized: true,
+//   name: 'sessionId',
+// }))
 
 // Express Messages Middleware
-app.use(require('connect-flash')())
-app.use(function(req, res, next){
-  res.locals.messages = require('express-messages')(req, res)
-  next()
-})
+// app.use(require('connect-flash')())
+// app.use(function(req, res, next){
+//   res.locals.messages = require('express-messages')(req, res)
+//   next()
+// })
 
 
 
