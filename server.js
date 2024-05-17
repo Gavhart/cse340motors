4364434a6de6d81d169cf49f79b9ec82644c1461
gavhart <har19076@byui.edu>
2024-05-16 22:22:29 -0600

    close

--- server.js
+++ server.js
@@ -26,23 +26,23 @@ const session = require("express-session")
 /* ***********************
  * Middleware
  * ************************/
-// app.use(session({
-//   store: new (require('connect-pg-simple')(session))({
-//     createTableIfMissing: true,
-//     pool,
-//   }),
-//   secret: process.env.SESSION_SECRET,
-//   resave: true,
-//   saveUninitialized: true,
-//   name: 'sessionId',
-// }))
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
 
 // Express Messages Middleware
-// app.use(require('connect-flash')())
-// app.use(function(req, res, next){
-//   res.locals.messages = require('express-messages')(req, res)
-//   next()
-// })
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
 
 
 
@@ -108,6 +108,6 @@ const host = process.env.HOST
 /* ***********************
  * Log statement to confirm server operation
  *************************/
-app.listen(port, host, () => {
+app.listen(port, host () => {
   console.log(`app listening on ${host}:${port}`)
 })
