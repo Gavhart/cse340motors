c31d1014bc0ce417829a9c7c8e3e2346e2bf3742
gavhart <har19076@byui.edu>
2024-05-16 22:25:07 -0600

    closer

--- server.js
+++ server.js
@@ -108,6 +108,6 @@ const host = process.env.HOST
 /* ***********************
  * Log statement to confirm server operation
  *************************/
-app.listen(port, host () => {
+app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
